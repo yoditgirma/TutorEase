a8b623493e70d41083043357529ea7764bd03c38
yoditgirma <149189296+yoditgirma@users.noreply.github.com>
2025-05-08 23:10:16 +0300

    Create index.js

---
+++ TutorialEase/frontend/js/index.js
@@ -0,0 +1,11 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+            observer.unobserve(entry.target); // Stop observing after showing
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach(el => observer.observe(el));
